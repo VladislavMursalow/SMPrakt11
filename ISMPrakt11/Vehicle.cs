551d345f76d10d5e22174c93c3a9b58c2ff484b6
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-02-08 10:48:39 +0200

    Added methods in Vehicle

--- ISMPrakt11/Vehicle.cs
+++ ISMPrakt11/Vehicle.cs
@@ -8,6 +8,38 @@ namespace ISMPrakt11
 {
     public abstract class Vehicle
     {
+        protected List<string> _visitedCities;
 
+        protected int _mileage;
+
+        public Vehicle()
+        {
+            _mileage = 0;
+            _visitedCities = new List<string>();
+        }
+
+        public void Ride(string city, int mileage) 
+        {
+            _visitedCities.Add(city);
+            if (mileage < 0) throw new Exception("Millage can`t be less than zero!");
+            _mileage += mileage;
+        }
+
+        public void Resetmileage() 
+        {
+            _mileage = 0;
+        }
+
+        public int GetTotalMileage() 
+        {
+            return _mileage;
+        }
+
+        public List<string> GetAllCities() 
+        {
+            return _visitedCities;
+        }
+
+        public abstract string GetData();
     }
 }
