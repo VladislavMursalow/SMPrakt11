68ffaaeb3b7af25ab775e31aa4d49b6bde2b71d3
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-02-08 10:35:15 +0200

    Created abstract class Vehicle.cs

---
+++ ISMPrakt11/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISMPrakt11
+{
+    public abstract class Vehicle
+    {
+
+    }
+}
