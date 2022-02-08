9f23b9eeb7c490b793874b2e2712ac2c2317f2b7
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-02-08 11:09:26 +0200

    Created class Car with start initialization

---
+++ ISMPrakt11/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISMPrakt11
+{
+    public class Car : Vehicle
+    {
+        public Car(string [] decriptions) : base(decriptions) {}
+
+        public override string GetData()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Car: (");
+
+            foreach (string s in _descriptions) 
+            {
+                sb.Append(s + " ");
+            }
+            sb.Append(") ");
+
+            return sb.ToString() + _mileage + "km";
+        }
+    }
+}
