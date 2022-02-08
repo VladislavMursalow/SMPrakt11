ff38a6308e3576244716da9d5dd4db5afa4f7911
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2022-02-08 11:13:06 +0200

    Created class Train

---
+++ ISMPrakt11/Train.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISMPrakt11
+{
+    public class Train : Vehicle
+    {
+        public Train(string[] decriptions) : base(decriptions) { }
+
+        public override string GetData()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Train: (");
+
+            foreach (string s in _descriptions)
+            {
+                sb.Append(s + " ");
+            }
+            sb.Append(") ");
+
+            return sb.ToString() + _mileage + "km";
+        }
+    }
+}
